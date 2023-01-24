13a8ab19d64b2f5a3bdecfc5ad3a50db95e1a6f7
lkrumpak <l.krumpak@student.vu.nl>
2023-01-24 20:41:23 +0100

    modified commands

--- groundTruth/init.sh
+++ groundTruth/init.sh
@@ -1,10 +1,6 @@
-# Pulls the dynamic-deadfunction-detector submodule
-git submodule update --init
-
-# Pulls the todomvc applications
-git pull https://github.com/NielsGrootObbink/todomvc.git
 
 # Runs npm install on all projects
+npm install
 npm --prefix ./todomvc install ./todomvc
 npm --prefix ./todomvc/tests install ./todomvc/tests
 npm --prefix ./dynamic-deadfunction-detector install ./dynamic-deadfunction-detector
