2eead4f0faebace9fe9e3ce1b9dcad309a8383ff
lkrumpak <l.krumpak@student.vu.nl>
2023-01-25 10:38:16 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
-# Install dependencies for LacunaV2 and LacunaV2-evaluator 
-npm install --prefix LacunaV2
-npm install --prefix LacunaV2-evaluator
 
 # Runs npm install on all projects
+npm --prefix ./scripts install ./scripts
 npm --prefix ./todomvc install ./todomvc
 npm --prefix ./todomvc/tests install ./todomvc/tests
+npm --prefix ./dynamic-deadfunction-detector install ./dynamic-deadfunction-detector
+
+npm --prefix ./lacuna_evaluation/LacunaV2 install ./lacuna_evaluation/LacunaV2
+npm --prefix ./lacuna_evaluation/LacunaV2-evaluator install ./lacuna_evaluation/LacunaV2-evaluator
+
+
+# apply todomvc diff 
+(cd todomvc && git apply ../scripts/todomvc.diff)
+
